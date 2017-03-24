46e492573330273b3ea298e5e55f3335d261301e
Richard Tier <rihard@highertier.com>
2017-03-24 16:53:39 +0000

    Add tab base template

--- profile/views.py
+++ profile/views.py
@@ -3,3 +3,8 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'landing-page.html'
+
+    def get_context_data(self):
+    	return {
+    		'sso_user_email': self.request.sso_user.email
+    	}
