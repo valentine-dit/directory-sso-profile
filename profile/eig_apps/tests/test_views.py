9a50154b36c349c8dd504c4b9e20d6b7db4f6c28
Richard Tier <rihard@highertier.com>
2017-04-03 11:31:08 +0100

    Add about tab url

---
+++ profile/eig_apps/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.core.urlresolvers import reverse
+
+from profile.eig_apps import views
+
+
+def test_about_view_exposes_context_and_template(client):
+    response = client.get(reverse('about'))
+
+    assert response.context_data['about_tab_classes'] == 'active'
+    assert response.template_name == [views.AboutView.template_name]
--- profile/eig_apps/views.py
+++ profile/eig_apps/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'landing-page.html'
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self):
+        return {
+            'about_tab_classes': 'active'
+        }
