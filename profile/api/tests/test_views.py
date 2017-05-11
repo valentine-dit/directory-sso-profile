3dffbd7e3175bf1eea02e3fa775876c60d6a5775
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-05-11 14:15:49 +0100

    handling client errors

--- profile/api/tests/test_views.py
+++ profile/api/tests/test_views.py
@@ -1,5 +1,7 @@
 from unittest.mock import patch
 
+from requests import HTTPError, Response
+from rest_framework import status
 from rest_framework.reverse import reverse
 
 
@@ -10,3 +12,19 @@ def test_external_supplier_get(mock_get_supplier_profile, client):
     url = reverse('api-external-supplier', kwargs={'sso_id': 2})
     response = client.get(url)
     assert response.json() == {'foo': 'bar'}
+
+
+@patch('profile.api.helpers.get_supplier_profile')
+def test_external_supplier_client_error(mock_get_supplier_profile, client):
+    client_response = Response()
+    client_response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    mock_get_supplier_profile.side_effect = HTTPError(
+        'Error',
+        response=client_response
+    )
+
+    url = reverse('api-external-supplier', kwargs={'sso_id': 2})
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert response.json() == 'Error'
--- profile/api/views.py
+++ profile/api/views.py
@@ -13,6 +13,7 @@ class ExternalSupplierAPIView(APIView):
     def get(self, request, sso_id, format=None):
         try:
             supplier = get_supplier_profile(sso_id=sso_id)
-        except HTTPError:
-            pass
+        except HTTPError as e:
+            # HTTPError doesn't have the message attribute!
+            return Response(str(e), status=e.response.status_code)
         return Response(supplier)
