b7bac7313709f8e0413b8af3b3e0fa45ef4504d8
richtier <rikatee@gmail.com>
2018-09-28 17:20:40 +0100

    Add sentry healthcheck

--- healthcheck/tests/test_backends.py
+++ healthcheck/tests/test_backends.py
@@ -30,30 +30,3 @@ def test_single_sign_on_ping_ok():
     backend.run_check()
 
     assert backend.pretty_status() == 'working'
-
-
-@patch('api_client.api_client.ping', Mock(side_effect=Exception('oops')))
-def test_api_ping_connection_error():
-    backend = backends.APIProxyBackend()
-    backend.run_check()
-
-    assert backend.pretty_status() == 'unavailable: (API proxy) oops'
-
-
-@patch('api_client.api_client.ping', Mock(return_value=Mock(status_code=500)))
-def test_api_ping_not_ok():
-    backend = backends.APIProxyBackend()
-    backend.run_check()
-
-    assert backend.pretty_status() == (
-        'unexpected result: api proxy returned 500 status code'
-    )
-
-
-@patch('api_client.api_client.ping',
-       Mock(return_value=Mock(status_code=200)))
-def test_api_ping_ok():
-    backend = backends.APIProxyBackend()
-    backend.run_check()
-
-    assert backend.pretty_status() == 'working'
