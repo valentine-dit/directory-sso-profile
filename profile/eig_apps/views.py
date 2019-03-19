daf5c051df0f3c976cfc12c5be1d69721f0b74a8
richtier <rikatee@gmail.com>
2019-03-19 21:42:31 +0000

    Remove redirect to about page

--- profile/eig_apps/views.py
+++ profile/eig_apps/views.py
@@ -1,5 +1,3 @@
-from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
 from django.views.generic import RedirectView, TemplateView
 
 from profile.eig_apps import constants
@@ -21,11 +19,3 @@ class AboutView(TemplateView):
         session_key = constants.HAS_VISITED_ABOUT_PAGE_SESSION_KEY
         self.request.session[session_key] = 'true'
         return super().get(*args, **kwargs)
-
-
-class RedirectToAboutPageMixin:
-    def dispatch(self, request, *args, **kwargs):
-        has_visited = constants.HAS_VISITED_ABOUT_PAGE_SESSION_KEY
-        if has_visited not in request.session:
-            return redirect(reverse('about'))
-        return super().dispatch(request, *args, **kwargs)
--- profile/soo/tests/test_views.py
+++ profile/soo/tests/test_views.py
@@ -1,5 +1,3 @@
-import http
-
 from django.core.urlresolvers import reverse
 
 
@@ -10,12 +8,3 @@ def test_selling_online_overseas_exposes_context(
     response = returned_client.get(url)
 
     assert response.context_data['soo_tab_classes'] == 'active'
-
-
-def test_selling_online_overseas_redirect_first_time_user(
-    sso_user_middleware, client
-):
-    response = client.get(reverse('selling-online-overseas'))
-
-    assert response.status_code == http.client.FOUND
-    assert response.get('Location') == reverse('about')
