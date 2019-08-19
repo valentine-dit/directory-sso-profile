6e92db19860b127c13dc1c21dc908d103d889c07
richtier <rikatee@gmail.com>
2019-08-19 18:10:38 +0100

    Add collaborator list admin view

--- profile/fab/tests/test_helpers.py
+++ profile/fab/tests/test_helpers.py
@@ -1,5 +1,9 @@
+from unittest import mock
+
+from directory_api_client import api_client
 import pytest
 
+from core.tests.helpers import create_response
 from profile.fab import helpers
 
 
@@ -11,3 +15,49 @@ def test_profile_parser_is_sole_trader(value, expected):
     parser = helpers.CompanyParser({'company_type': value})
 
     assert parser.is_sole_trader is expected
+
+
+def test_profile_parser_no_data_serialize_for_form():
+    parser = helpers.CompanyParser({})
+
+    assert parser.serialize_for_form() == {}
+
+
+def test_profile_parser_no_data_serialize_for_template():
+    parser = helpers.CompanyParser({})
+
+    assert parser.serialize_for_template() == {}
+
+
+@mock.patch.object(api_client.supplier, 'retrieve_profile')
+def test_get_supplier_profile(mock_retrieve_profile):
+    data = {'name': 'Foo Bar'}
+    mock_retrieve_profile.return_value = create_response(200, data)
+
+    profile = helpers.get_supplier_profile('1234')
+
+    assert mock_retrieve_profile.call_count == 1
+    assert mock_retrieve_profile.call_args == mock.call('1234')
+    assert profile == data
+
+
+@mock.patch.object(api_client.supplier, 'retrieve_profile')
+def test_get_supplier_profile_not_found(mock_retrieve_profile):
+    mock_retrieve_profile.return_value = create_response(404)
+
+    profile = helpers.get_supplier_profile('1234')
+
+    assert mock_retrieve_profile.call_count == 1
+    assert mock_retrieve_profile.call_args == mock.call('1234')
+    assert profile is None
+
+
+@mock.patch.object(api_client.company, 'retrieve_private_profile')
+def test_get_company_profile_not_found(mock_retrieve_private_profile):
+    mock_retrieve_private_profile.return_value = create_response(404)
+
+    profile = helpers.get_company_profile('1234')
+
+    assert mock_retrieve_private_profile.call_count == 1
+    assert mock_retrieve_private_profile.call_args == mock.call('1234')
+    assert profile is None
