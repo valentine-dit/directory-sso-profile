b2c390d0e4875754eb1b4e32517696d3684ec030
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-09-12 14:11:51 +0100

    fix url string

--- conf/urls.py
+++ conf/urls.py
@@ -321,13 +321,13 @@ urlpatterns = [
     url(
         r'^find-a-buyer/(?P<path>[\w\-/]*)/$',
         RedirectView.as_view(
-            url=urls.domestic.SINGLE_SIGN_ON_PROFILE + 'profile/business-profile/%(path)s', query_string=True
+            url=urls.domestic.SINGLE_SIGN_ON_PROFILE / 'profile/business-profile/%(path)s', query_string=True
         ),
     ),
     url(
         r'^find-a-buyer/',
         RedirectView.as_view(
-            url=urls.domestic.SINGLE_SIGN_ON_PROFILE + 'profile/business-profile/',
+            url=urls.domestic.SINGLE_SIGN_ON_PROFILE / 'profile/business-profile/',
             query_string=True),
     ),
 ]
